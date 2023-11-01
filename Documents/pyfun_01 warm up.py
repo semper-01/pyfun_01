7087090d4e54f6abf3d2a21a5c8cc08111c52aff
leonce <mleonce99@gmail.com>
2023-11-01 15:03:31 +0200

    user's insights

--- Documents/pyfun_01 warm up.py
+++ Documents/pyfun_01 warm up.py
@@ -1,8 +1,4 @@
-num_str="42"
-num_int=int(num_str)
-print(f"{num_int: }is {type(num_int)}")
-
-num_float=float(3.14)
-num_str2= str(num_float)
-print(f"{num_str2: }is {type(num_str2)}")
-
+name=input("enter your name: ")
+age=input("enter your age: ")
+print(f"hello,{name}!")
+print(f"you are {age}years old.")
